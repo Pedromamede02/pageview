c9f5b5bf13a20f73e379810e7eb7d5ec8511c4f6
Pedromamede02 <102523813+Pedromamede02@users.noreply.github.com>
2022-06-28 16:38:11 -0300

    commit
    O primeiro commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.area-1', {duration:3000});
+
+
+sr.reveal('.area-2', {
+    rotate: {x:0, y: 80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.area-3', {
+    rotate: {x:100, y:1000, z:0},
+    duartion: 2000
+});
+
